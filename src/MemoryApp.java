0ff7d0a2f7ba8725f3c72077ddb2e96bd657df0c
Jürgen Hahn <juergen.hahn@ur.de>
2022-01-24 12:35:24 +0100

    pushed starter

---
+++ src/MemoryApp.java
@@ -0,0 +1,17 @@
+import de.ur.mi.oop.app.GraphicsApp;
+import de.ur.mi.oop.launcher.GraphicsAppLauncher;
+
+public class MemoryApp extends GraphicsApp {
+
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void draw() {
+    }
+
+    public static void main(String[] args) {
+        GraphicsAppLauncher.launch();
+    }
+}
